00d70c806d092e6fdbf8a6f36120e33f353bdb51
Pavan Maganti <pavanmaganti9@gmail.com>
2019-03-01 11:54:59 +0530

    excel

---
+++ excel_read.py
@@ -0,0 +1,35 @@
+import xlrd 
+  
+# Give the location of the file 
+loc = ("DS_ae.xlsx") 
+  
+# To open Workbook 
+wb = xlrd.open_workbook(loc) 
+sheet = wb.sheet_by_index(0) 
+  
+# For row 0 and column 0 
+sheet.cell_value(0, 0)
+
+#print(sheet.cell_value(0, 0))
+
+'''Extract number of rows'''
+#print(sheet.nrows) 
+
+'''Extract number of columns'''
+#print(sheet.ncols)
+
+'''Extracting all columns name'''
+for i in range(sheet.ncols): 
+    print(sheet.cell_value(0, i))
+
+'''Extract first column'''	
+#for i in range(sheet.nrows): 
+#    print(sheet.cell_value(i, 0)) 
+
+
+'''Extract a particular row value'''	
+print(sheet.row_values(1))
+
+'''Get all the rows in excel file'''
+#for i in range(sheet.nrows): 
+#    print(sheet.row_values(i, 0)) 
