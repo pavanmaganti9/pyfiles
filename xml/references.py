6f663f97cb034dcee0a38fdaaf4f7323e32bb9fb
Pavan Maganti <pavanmaganti9@gmail.com>
2019-04-18 23:30:29 +0530

    Update references.py

--- xml/references.py
+++ xml/references.py
@@ -4,3 +4,9 @@ https://www.datacamp.com/community/tutorials/python-xml-elementtree
   Creating csv
   
 https://docs.python.org/2.7/library/csv.html
+
+
+Collections in py
+
+https://stackabuse.com/introduction-to-pythons-collections-module/
+  
