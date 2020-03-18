91399a59c02bee3e92f754fb9b1a256a4f78527a
Pavan Maganti <pavanmaganti9@gmail.com>
2020-03-18 16:54:06 +0530

    Update apache.py

--- apache.py
+++ apache.py
@@ -3,3 +3,18 @@ print "Content-type: text/html"
 print
 print ("Pavan")
 print "<br><br>"
+
+$headers = getallheaders();
+$payload = file_get_contents("php://input");
+$webhook = new AuthnetWebhook(AUTHNET_SIGNATURE, $payload, $headers);
+if ($webhook-&gt;isValid()) {
+    // Access notification values
+}
+
+$headers = getallheaders();
+$payload = file_get_contents("php://input");
+$webhook = new AuthnetWebhook(AUTHNET_SIGNATURE, $payload, $headers);
+if ($webhook-&gt;isValid()) {
+    // Get the transaction ID
+    $transactionId = $webhook-&gt;payload-&gt;id;
+}
